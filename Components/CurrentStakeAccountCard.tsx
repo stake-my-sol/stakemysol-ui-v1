4ab2214a434b302114d661d2df4d3e9c2e3f38a8
Vegan Cat <mmostafavi247@gmail.com>
2022-09-16 01:42:30 +0430

    Update stakes page
    - Show Current Stake Accounts in stakes page

---
+++ Components/CurrentStakeAccountCard.tsx
@@ -0,0 +1,13 @@
+import { Paper, Typography } from "@mui/material";
+
+interface CurrentStakeAccountCardProps {}
+
+function CurrentStakeAccountCard({}: CurrentStakeAccountCardProps) {
+  return (
+    <Paper elevation={3}>
+      <Typography>Current Stake Account Card</Typography>
+    </Paper>
+  );
+}
+
+export default CurrentStakeAccountCard;
--- pages/stakes.tsx
+++ pages/stakes.tsx
@@ -15,7 +15,7 @@ import {
   Lockup,
 } from "@solana/web3.js";
 import { useState, useEffect } from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { STAKE_ACCOUNT_SEED_PREFIX } from "../Constants";
 import { IUIStakeAccount, IPotentialSMSPubkeys } from "../@types";
 
@@ -42,10 +42,6 @@ const Stakes: NextPage = () => {
           },
         ],
       });
-      console.log(
-        "🚀 ~ file: stakes.tsx ~ line 41 ~ fetchCurrentStakeAccounts ~ res",
-        res,
-      );
 
       let parsedStakeAccounts: IUIStakeAccount[] = [];
       for (let i = 0; i < res.length; i++) {
@@ -56,7 +52,7 @@ const Stakes: NextPage = () => {
           balance:
             res[i].account.lamports -
             parsedData.parsed.info.meta.rentExemptReserve,
-          status: parsedData.parsed.info.type,
+          status: parsedData.parsed.type,
           withdrawAuthority: new PublicKey(
             parsedData.parsed.info.meta.authorized.withdrawer,
           ),
@@ -74,68 +70,48 @@ const Stakes: NextPage = () => {
     fetchCurrentStakeAccounts();
   }, [publicKey, connection, stakeProgramId, seed]);
 
-  const onClick = async () => {
-    if (!publicKey) throw new WalletNotConnectedError();
+  let accounts = null;
 
-    // Create a keypair for our stake account
-    const stakeAccount = await PublicKey.createWithSeed(
-      publicKey,
-      seed,
-      stakeProgramId,
+  if (currentStakeAccounts.length > 0) {
+    accounts = (
+      <Grid container>
+        {currentStakeAccounts.map((stakeAccount, index) => {
+          return (
+            <Grid sx={{ my: 2 }} item xs={12} md={6} key={index}>
+              <Box sx={{ textAlign: "center" }}>
+                <Typography variant="h6">#{index + 1}</Typography>
+                {/* <Typography variant="h6">
+                      Public Key: {stakeAccount.publicKey.toBase58()}
+                    </Typography> 
+                  */}
+                <Typography>
+                  Balance: {stakeAccount.balance / LAMPORTS_PER_SOL} SOL
+                </Typography>
+                <Typography>Status: {stakeAccount.status}</Typography>
+                <Typography>
+                  Withdraw Authority:{" "}
+                  {stakeAccount.withdrawAuthority.toBase58() ===
+                  publicKey!.toBase58()
+                    ? "You"
+                    : stakeAccount.withdrawAuthority.toBase58()}
+                </Typography>
+                <Typography>
+                  Stake Authority:{" "}
+                  {stakeAccount.stakeAuthority.toBase58() ===
+                  publicKey!.toBase58()
+                    ? "You"
+                    : stakeAccount.stakeAuthority.toBase58()}
+                </Typography>
+              </Box>
+            </Grid>
+          );
+        })}
+      </Grid>
     );
-
-    // Calculate how much we want to stake
-    const minimumRent = await connection.getMinimumBalanceForRentExemption(
-      StakeProgram.space,
-    );
-    const amountUserWantsToStake = LAMPORTS_PER_SOL * 0.5; // This is can be user input. For now, we'll hardcode to 0.5 SOL
-    const amountToStake = minimumRent + amountUserWantsToStake;
-
-    // Setup a transaction to create our stake account
-    // Note: `StakeProgram.createAccount` returns a `Transaction` preconfigured with the necessary `TransactionInstruction`s
-    const createStakeAccountTx = StakeProgram.createAccountWithSeed({
-      fromPubkey: publicKey,
-      stakePubkey: stakeAccount,
-      basePubkey: publicKey,
-      seed: seed,
-      authorized: new Authorized(publicKey, publicKey), // Here we set two authorities: Stake Authority and Withdrawal Authority. Both are set to our
-      lamports: amountToStake,
-    });
-
-    const {
-      context: { slot: minContextSlot },
-      value: { blockhash, lastValidBlockHeight },
-    } = await connection.getLatestBlockhashAndContext();
-
-    const signature = await sendTransaction(createStakeAccountTx, connection, {
-      minContextSlot,
-    });
-
-    await connection.confirmTransaction({
-      blockhash,
-      lastValidBlockHeight,
-      signature,
-    });
-
-    // Check our newly created stake account balance. This should be 0.5 SOL.
-    let stakeBalance = await connection.getBalance(stakeAccount);
-    console.log(
-      `Stake account balance: ${stakeBalance / LAMPORTS_PER_SOL} SOL`,
-    );
-
-    // Verify the status of our stake account. This will start as inactive and will take some time to activate.
-    let stakeStatus = await connection.getStakeActivation(stakeAccount);
-    console.log(`Stake account status: ${stakeStatus.state}`);
-
-    setSeed((Number(seed) + 1).toString());
-  };
+  }
 
   return (
-    <Box>
-      <Typography>
-        You have {currentStakeAccounts.length} at the moment
-      </Typography>
-    </Box>
+    <Box>{accounts === null ? <Box>Nothing to show!</Box> : accounts}</Box>
   );
 };
 
