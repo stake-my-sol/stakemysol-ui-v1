8a6d015741f7e158f89a4470d7533fdceb40e008
Vegan Cat <mmostafavi247@gmail.com>
2022-11-19 18:16:35 +0330

    Update Current Stakes Table

--- Components/StakesPage/CurrentStakesTable.tsx
+++ Components/StakesPage/CurrentStakesTable.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { alpha } from "@mui/material/styles";
 import {
   TableRow,
   Button,
@@ -10,50 +9,61 @@ import {
   TableContainer,
   TableHead,
   TableSortLabel,
-  Toolbar,
-  Typography,
   Paper,
 } from "@mui/material";
 import { visuallyHidden } from "@mui/utils";
 
 interface Data {
-  calories: number;
-  carbs: number;
-  fat: number;
-  name: string;
-  protein: number;
+  address: string;
+  value: string;
+  profit: string;
+  state: string;
+  actions: string;
 }
 
 function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+  address: string,
+  value: string,
+  profit: string,
+  state: string,
+  actions: string,
 ): Data {
   return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
+    address,
+    value,
+    profit,
+    state,
+    actions,
   };
 }
 
 const rows = [
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Honeycomb", 408, 3.2, 87, 6.5),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  createData("KitKat", 518, 26.0, 65, 7.0),
-  createData("Lollipop", 392, 0.2, 98, 0.0),
-  createData("Marshmallow", 318, 0, 81, 2.0),
-  createData("Nougat", 360, 19.0, 9, 37.0),
-  createData("Oreo", 437, 18.0, 63, 4.0),
+  createData(
+    "Pst...fst",
+    "305 SOL",
+    "3.7 SOL",
+    "Active",
+    ["Claim", "Transfer"].join(","),
+  ),
+  createData(
+    "h4x...bft",
+    "3.5M SOL",
+    "30k SOL",
+    "Active",
+    ["Claim", "Transfer"].join(","),
+  ),
+  // createData("Donut", 452, 25.0, 51, 4.9),
+  // createData("Eclair", 262, 16.0, 24, 6.0),
+  // createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  // createData("Gingerbread", 356, 16.0, 49, 3.9),
+  // createData("Honeycomb", 408, 3.2, 87, 6.5),
+  // createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  // createData("Jelly Bean", 375, 0.0, 94, 0.0),
+  // createData("KitKat", 518, 26.0, 65, 7.0),
+  // createData("Lollipop", 392, 0.2, 98, 0.0),
+  // createData("Marshmallow", 318, 0, 81, 2.0),
+  // createData("Nougat", 360, 19.0, 9, 37.0),
+  // createData("Oreo", 437, 18.0, 63, 4.0),
 ];
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
@@ -106,34 +116,34 @@ interface HeadCell {
 
 const headCells: readonly HeadCell[] = [
   {
-    id: "name",
+    id: "address",
     numeric: false,
     disablePadding: true,
-    label: "Dessert (100g serving)",
+    label: "Address",
   },
   {
-    id: "calories",
-    numeric: true,
+    id: "value",
+    numeric: false,
     disablePadding: false,
-    label: "Calories",
+    label: "Value",
   },
   {
-    id: "fat",
-    numeric: true,
+    id: "profit",
+    numeric: false,
     disablePadding: false,
-    label: "Fat (g)",
+    label: "Profit",
   },
   {
-    id: "carbs",
-    numeric: true,
+    id: "state",
+    numeric: false,
     disablePadding: false,
-    label: "Carbs (g)",
+    label: "State",
   },
   {
-    id: "protein",
-    numeric: true,
+    id: "actions",
+    numeric: false,
     disablePadding: false,
-    label: "Protein (g)",
+    label: "Actions",
   },
 ];
 
@@ -160,8 +170,8 @@ function EnhancedTableHead(props: EnhancedTableProps) {
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
-            padding={headCell.disablePadding ? "none" : "normal"}
+            align="left"
+            padding="normal"
             sortDirection={orderBy === headCell.id ? order : false}
           >
             <TableSortLabel
@@ -183,11 +193,9 @@ function EnhancedTableHead(props: EnhancedTableProps) {
   );
 }
 
-interface EnhancedTableToolbarProps {}
-
 export default function EnhancedTable() {
   const [order, setOrder] = React.useState<Order>("asc");
-  const [orderBy, setOrderBy] = React.useState<keyof Data>("calories");
+  const [orderBy, setOrderBy] = React.useState<keyof Data>("value");
 
   const handleRequestSort = (
     event: React.MouseEvent<unknown>,
@@ -218,25 +226,18 @@ export default function EnhancedTable() {
               rows.slice().sort(getComparator(order, orderBy)) */}
               {stableSort(rows, getComparator(order, orderBy)).map(
                 (row, index) => {
-                  const labelId = `enhanced-table-checkbox-${index}`;
-
                   return (
-                    <TableRow hover tabIndex={-1} key={row.name}>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">
-                        {/* {row.protein} */}
-                        <Button variant="contained">Edit</Button>
-                        <Button variant="contained">Edit</Button>
+                    <TableRow hover tabIndex={-1} key={row.address}>
+                      <TableCell align="left">{row.address}</TableCell>
+                      <TableCell align="left">{row.value}</TableCell>
+                      <TableCell align="left">{row.profit}</TableCell>
+                      <TableCell align="left">{row.state}</TableCell>
+                      <TableCell align="left">
+                        {row.actions.split(",").map((action) => (
+                          <Button variant="contained" key={action}>
+                            {action}
+                          </Button>
+                        ))}
                       </TableCell>
                     </TableRow>
                   );
